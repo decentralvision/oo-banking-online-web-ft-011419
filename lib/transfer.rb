d71f86fe314d6b6222929d98d09222141feb048a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 02:17:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,8 @@
 class Transfer
-  attr_reader :sender, :receiver
+  attr_reader :sender, :receiver, :status
   def initialize(sender, receiver)
     @sender = sender
     @receiver = sender
+    @status = pending
   end
 end
