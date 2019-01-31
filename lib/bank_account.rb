deba25d0e81bdf28b4e42c5a8587b4ced620739f
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 02:11:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,4 +6,10 @@ class BankAccount
     @balance = 1000
     @status = 'open'
   end
+  def deposit(money)
+    @balance += money
+  end
+  def self.display_balance
+    "Your balance is $#{@balance}"
+  end
 end
