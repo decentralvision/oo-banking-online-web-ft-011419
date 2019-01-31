e995194c70adebe0bb4c9814ffbc18fba3385a38
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 02:18:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,9 @@
 class Transfer
-  attr_reader :sender, :receiver, :status
-  def initialize(sender, receiver)
+  attr_reader :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = sender
     @status = 'pending'
+    @amount = amount
   end
 end
