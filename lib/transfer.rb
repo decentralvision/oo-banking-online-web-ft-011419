465d05c959a63a7ac1de45ef950d79e89c34d33e
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 02:26:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
       @receiver.balance += @amount
       @status = 'complete'
     else
-      'Transfer is not valid'
+      'Transaction rejected. Please check your account balance.'
     end
   end
 end
