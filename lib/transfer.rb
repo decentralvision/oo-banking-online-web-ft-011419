4cf7bb8a44b440dddde987e64d6597c5490987cf
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-31 02:28:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,12 +10,19 @@ class Transfer
     @sender.valid? && @receiver.valid? && @status == 'pending' && @sender.balance >= @amount
   end
   def execute_transaction
-    if self.valid? do
+    if self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
     else
       'Transaction rejected. Please check your account balance.'
+    end
+  end
+  def reverse_transfer
+    if @status = 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
     end
   end
 end
